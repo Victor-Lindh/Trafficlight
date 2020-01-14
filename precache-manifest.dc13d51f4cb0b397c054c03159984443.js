f813408fda7f5c8e4f5e54260c8234920e0391d8
Victor Lindh <Victor.Lindh@yh.nackademin.se>
2020-01-14 10:20:01 +0100

    Updates

---
+++ precache-manifest.dc13d51f4cb0b397c054c03159984443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed78bb5c541feae2e0104b676907c99",
+    "url": "/Trafficlight/index.html"
+  },
+  {
+    "revision": "f2637a02b23e91ced3e3",
+    "url": "/Trafficlight/static/css/main.0b592565.chunk.css"
+  },
+  {
+    "revision": "b4f155a5948f51b64a55",
+    "url": "/Trafficlight/static/js/2.abffd684.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Trafficlight/static/js/2.abffd684.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2637a02b23e91ced3e3",
+    "url": "/Trafficlight/static/js/main.f2c2d5c1.chunk.js"
+  },
+  {
+    "revision": "127244deac85324662ef",
+    "url": "/Trafficlight/static/js/runtime-main.ec522439.js"
+  }
+]);
